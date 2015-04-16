6469a7fb43ff62449f65a7668238e014a47f42a7
Chris Jacobs <zonk1024@gmail.com>
2015-04-16 12:42:20 -0700

    much better

--- redis_dict.py
+++ redis_dict.py
@@ -11,32 +11,32 @@ class RedisDict(object):
         self.prefix = prefix
         self.host = host
         self.port = port
-        self.r = self._r()
 
-    def _r(self):
+    @property
+    def r(self):
         success = False
         while not success:
             try:
-                success = self.r.ping()
+                success = self._r.ping()
             except (redis.ConnectionError, AttributeError):
-                self.r = redis.Redis(self.host, self.port)
-        return self.r
+                self._r = redis.Redis(self.host, self.port)
+        return self._r
 
     def pickle_key(self, key='*'):
-        #hash(key)  # throw an error if you must
-        return pickle.dumps('{}{}'.format(self.prefix, key))
+        hash(key)  # throw an error if you must
+        return '{}{}'.format(self.prefix, pickle.dumps(key))
 
     def unpickle_key(self, key):
-        return pickle.loads(key)[len(self.prefix):]
+        return pickle.loads(key[len(self.prefix):])
 
     def pickle_value(self, value):
         return pickle.dumps(value)
 
     def unpickle_value(self, value):
-        return value if value is None else pickle.loads(value)
+        return None if value is None else pickle.loads(value)
 
     def len(self):
-        return len(self.keys())
+        return len(self._keys())
 
     def clear(self):
         for key in self.keys():
@@ -74,8 +74,10 @@ class RedisDict(object):
         return self.__iter__()
 
     def keys(self):
-        return sorted(self.unpickle_key(key)
-                      for key in self._r().keys(self.pickle_key()))
+        return [self.unpickle_key(key) for key in self.r.keys('{}*'.format(self.prefix))]
+
+    def sorted_keys(self):
+        return sorted(self._keys())
 
     def itervalues(self):
         return (self.__getitem__(key) for key in self.__iter__())
@@ -90,16 +92,16 @@ class RedisDict(object):
         return list(self.iteritems())
 
     def __getitem__(self, key):
-        return self.unpickle_value(self._r().get(self.pickle_key(key)))
+        return self.unpickle_value(self.r.get(self.pickle_key(key)))
 
     def __setitem__(self, key, value):
         if value is None:
             self.__delitem__(key)
             return
-        self._r().set(self.pickle_key(key), self.pickle_value(value))
+        self.r.set(self.pickle_key(key), self.pickle_value(value))
 
     def __delitem__(self, key):
-        self._r().delete(self.pickle_key(key))
+        self.r.delete(self.pickle_key(key))
 
     def __contains__(self, key):
         return key in self.keys()
@@ -108,14 +110,14 @@ class RedisDict(object):
         keys = self.keys()
         for key in keys:
             if keys != self.keys():
-                raise RuntimeError("dictionary changed size during iteration")
+                raise RuntimeError("RedisDict changed size during iteration")
             yield key
 
     def __reversed__(self):
         keys = self.keys()
         for key in keys[::-1]:
             if keys != self.keys():
-                raise RuntimeError("dictionary changed size during iteration")
+                raise RuntimeError("RedisDict changed size during iteration")
             yield key
 
     def __str__(self):
@@ -127,84 +129,46 @@ class RedisDict(object):
     def __dict__(self):
         return dict(self.items())
 
+    def __eq__(self, other):
+        if type(other) not in (dict, RedisDict):
+            return False
+        for k in self:
+            if other[k] != self[k]:
+                return False
+        return True
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
 
 if __name__ == '__main__':
-    gd = RedisDict()
-    gd2 = RedisDict()
+    # dictionary check
+    d = {}
     rd = RedisDict()
-    print 'init'
-    print 'gd', gd
-    print 'gd2', gd2
-    print 'rd', rd
-    print
-
-    del(gd['bob'])
-    del(rd['bob'])
-    print 'del(dict[\'bob\'])'
-    print 'gd', gd
-    print 'gd2', gd2
-    print 'rd', rd
-    print
-
-    print 'It works, but they don\'t seem to show up without a direct call'
-    # hence the line 26 comment
-    gd[{'x':'Y'}] = {'z': '0'}
-    rd[{'x':'Y'}] = {'z': '0'}
-    print 'VOODOO!'
-    print "gd[{'x':'Y'}] = {'z': '0'}"
-    print "rd[{'x':'Y'}] = {'z': '0'}"
-    print 'I SEE YOU!'
-    print "gd[{'x':'Y'}]", gd[{'x':'Y'}]
-    print "rd[{'x':'Y'}]", rd[{'x':'Y'}]
-    print 'WTF!?'
-    print 'gd', gd
-    print 'rd', rd
-    print
-
-    gd['bob'] = 'BOB'
-    gd[(1, 2)] = {'a':'A', 'b':[1, 2, '3', '4'], 'c':{'d': 'E', 'f': {'g': 'H'}}}
-    rd['bob'] = 'BOB'
-    rd[(1, 2)] = {'a':'A', 'b':[1, 2, '3', '4'], 'c': {'d': 'E', 'f': {'g': 'H'}}}
-
-    print 'populate'
-    print 'gd', gd
-    print 'gd2', gd2
-    print 'rd', rd
-    print
-
-    print 'iterator checks'
-    print 'list(gd.iterkeys())', list(gd.iterkeys())
-    print 'list(gd.itervalues())', list(gd.itervalues())
-    print 'list(gd.iteritems())', list(gd.iteritems())
-    print
-
-    print 'get(\'bob\')'
-    print 'gd', gd.get('bob')
-    print 'gd2', gd2.get('bob')
-    print 'rd', rd.get('bob')
-    print
-
-    gd.clear()
     rd.clear()
-    print 'clear()'
-    print 'gd', gd
-    print 'gd2', gd2
-    print 'rd', rd
-    print
 
-    gd['bob'] = 'BOB'
-    gd[(1, 2)] = {'a':'A', 'b':[1, 2, '3', '4']}
-    rd['bob'] = 'BOB'
-    rd[(1, 2)] = {'a':'A', 'b':[1, 2, '3', '4']}
-    print 'populate again'
-    print 'gd', gd
-    print 'gd2', gd2
-    print 'rd', rd
-    print
+    d['foo'] = 5
+    rd['foo'] = 5
 
-    print '\'bob\' in gd', 'bob' in gd
-    print '\'bob\' in rd', 'bob' in rd
-    print
+    d['bar'] = [1, 2, 3]
+    rd['bar'] = [1, 2, 3]
 
-    print '\'bob3\' in gd', 'bob3' in gd
-    print '\'bob3\' in rd', 'bob3' in rd
+    d['baz'] = {}
+    rd['baz'] = {}
+
+    d[(0, 1)] = {'1': 1, '2': 2}
+    rd[(0, 1)] = {'1': 1, '2': 2}
+
+    assert set(d.keys()) == set(rd.keys())
+
+    for k in rd:
+        assert d[k] == rd[k]
+
+    assert d == rd
+
+    rd.clear()
+    rd.update(d.items())
+
+    assert d == rd
+
+    rd.clear()
